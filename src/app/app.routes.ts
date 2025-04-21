6c4cb468f4a6876c6c1891138540c16703359e75
anaghav2023 <124548794+anaghav2023@users.noreply.github.com>
2025-04-21 17:19:52 +0530

    builder integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
+// app.routes.ts
+
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { Test1Component } from './test-1/test-1.component';
-import { Test2Component } from './test-2/test-2.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-{ path: '', component: HomeComponent },
-{ path: 'test-1', component: Test1Component},
-{ path: 'test-2', component: Test2Component },
+  {
+    path: '**',
+    component: LandingPageComponent,
+  },
 ];
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,37 @@
+// landing-page.component.ts
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { fetchOneEntry, type BuilderContent } from '@builder.io/sdk-angular';
+import { Content } from '@builder.io/sdk-angular';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [Content, CommonModule],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css',
+})
+export class LandingPageComponent {
+  // Add your Public API Key, specify the page model, and initialize the content variable
+  apiKey = '3f3ebbcc055e4b3dbf539daa67c4a45e';
+  model = 'page';
+  content: BuilderContent | null = null;
+
+  async ngOnInit() {
+    const urlPath = window.location.pathname || '';
+    // fetch the content for the current page based on the current URL
+    const content = await fetchOneEntry({
+      apiKey: this.apiKey,
+      model: this.model,
+      userAttributes: {
+        urlPath,
+      },
+    });
+
+    if (!content) {
+      return;
+    }
+
+    this.content = content;
+  }
+}
